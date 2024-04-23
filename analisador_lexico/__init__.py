a0bea432d97487117dfc155edea64b4ebb6efd17
laraesquivel <laraesquivelsantos@gmail.com>
2024-04-23 12:36:04 -0300

    a

--- analisador_lexico/__init__.py
+++ analisador_lexico/__init__.py
@@ -5,11 +5,19 @@ Alisson Rodrigues
 Lara Esquivel 
 
 
-Favor seguir as instruções abaixo:
+Favor seguir as instruções abaixo para executar o programa:
+
+Dentro da pasta execute
 
 python -m venv venv ou py -m venv venv
 
+Caso seja Linux:
+
 source venv/bin/activate
+
+Em caso de windows, retire o comando source, troque a pasta bin por Scripts e invertas as barras
+
+Por fim execute
 
 python _init_.py
 
